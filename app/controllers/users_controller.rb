955749030bffcb37c675bd1ab1db26042885fd61
I <tranduyphong@Trans-MacBook-Pro.local>
2016-03-17 19:55:39 +0700

    Finish Sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,18 @@ class UsersController < ApplicationController
   def index
     @users = User.all.shuffle
   end
+
+  def create
+  	@user = User.create user_params
+  	if @user.persisted?
+  		redirect_to users_path
+  	else
+  		render 'new'
+  	end
+  end
+
+  def user_params
+  	params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
 end
